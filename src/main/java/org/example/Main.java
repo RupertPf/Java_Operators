66c63709dc5ac797723bf4c88b886bcbf1ec1399
Rupert Pfiser <rupert.pfister@gmail.com>
2023-07-18 18:03:41 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Dvision
+        System.out.println("Das Ergebnis der Division ist: " + (7 / 2));
+        System.out.println("Das Ergebnis2 der Division ist: " + (7.0 / 2));
+        // Operationen auf characters (Buschstaben)
+        System.out.println("Buchstabe a: " + ((int)'a'));
+        System.out.println("a + 1: " + ('a' + 1));
+        System.out.println("a + 1: " + ((char)('a' + 1)));
+        // Aufgabe
+        int a = 21;
+        //System.out.println(++a);
+        //System.out.println(a++);
+        //System.out.println(a%9);
+        System.out.println(a<3);
+        int a2 = (int) 92.4/3;
+        System.out.println(a2);
+
+    }
+}
